29c1618e2b2cb714b6ec82d1d273309f3cd6fe4f
Ángel H.O <angel.hdz.ojd@gmail.com>
2023-10-15 21:12:39 +0100

    At the first we create the class, and the constructor.

---
+++ src/Person/main.java
@@ -0,0 +1,11 @@
+package Person;
+
+public class main {
+    public String name;
+    public String surname;
+
+    public main(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+}
